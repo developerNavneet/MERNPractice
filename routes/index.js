964296399615459785de8477b05c7825018ac89d
dj-ds <dilip@doomshell.com>
2022-02-18 08:57:28 +0530

    Router bug fixed

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,8 @@ const book = require('./bookRoutes')
 const users = require('./usersRoutes')
 const express = require('express')
 const router = express.Router()
+
+router.use('/api/v1/books', book);
+router.use('/api/v1/users', users);
+
+module.exports = router;
