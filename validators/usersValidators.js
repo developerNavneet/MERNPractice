248d8def215f678fcf2507f967cbd3f8e44f964d
dj-ds <dilip@doomshell.com>
2022-03-03 09:05:57 +0530

    Token Decodes

--- validators/usersValidators.js
+++ validators/usersValidators.js
@@ -1,19 +1,17 @@
 const { celebrate, Joi } = require("celebrate");
 
 module.exports.createUser = celebrate({
-  body: Joi.object().options({ abortEarly: false }).keys({
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required(),
-  }),
+    body: Joi.object().options({ abortEarly: false }).keys({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+    }),
 });
 
 module.exports.loginUser = celebrate({
-  body: Joi.object()
-    .options({ abortEarly: false })
-    .keys({
-      email: Joi.string().required().email({ minDomainSegments: 2 }),
-      password: Joi.string().required(),
+    body: Joi.object().options({ abortEarly: false }).keys({
+        email: Joi.string().required(),
+        password: Joi.string().required(),
     }),
 });
