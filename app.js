a232ec99cf7995d722a55e3206adddfd0ceb457f
dj-ds <dilip@doomshell.com>
2022-03-23 08:34:51 +0530

    File Upload Done

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose.connection.on("open", () => console.log("Successfully connected "));
 app.use(cors()); // Middleware call
 
 app.use(express.json());
+//app.use(express.static("./images"));
 
 app.use(errors());
 
